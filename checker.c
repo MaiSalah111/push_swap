6751a474f0092d48d9cc2ce29c7cd80db045d1ec
MaiSalah111 <158178517+MaiSalah111@users.noreply.github.com>
2024-02-27 10:44:04 +0000

    Eliminated_Leaks

--- checker.c
+++ checker.c
@@ -14,29 +14,74 @@
 
 int	main(int ac, char **av)
 {
-	t_stack	*a;
-	t_stack	*b;
-	char	*next_line;
-	int		len;
+    t_stack	*a;
+    t_stack	*b;
+    char	*next_line;
+    char    **split_av = NULL;
+    int		len;
+    int     i;
 
-	a = NULL;
-	b = NULL;
-	next_line = NULL;
-	if (ac == 1)
-		return (1);
-	else if (ac == 2)
-		av = split(av[1], ' ');
-	init_stack_a(&a, av + 1);
-	len = stack_len(a);
-	next_line = (get_next_line(0, next_line));
-	while (next_line)
-	{
-		apply_command(&a, &b, next_line);
-		next_line = (get_next_line(0, next_line));
-	}
-	if (stack_sorted(a) && stack_len(a) == len)
-		write(1, "OK\n", 3);
-	else
-		write(1, "KO\n", 3);
-	free(a);
+    a = NULL;
+    b = NULL;
+    next_line = NULL;
+    if (ac == 1)
+        return (1);
+    else if (ac == 2) {
+        split_av = split(av[1], ' ');
+        av = split_av;
+    }
+    init_stack_a(&a, av + 1);
+    len = stack_len(a);
+    next_line = (get_next_line(0, next_line));
+    while (next_line)
+    {
+        apply_command(&a, &b, next_line);
+        next_line = (get_next_line(0, next_line));
+    }
+    if (stack_sorted(a) && stack_len(a) == len)
+        write(1, "OK\n", 3);
+    else
+        write(1, "KO\n", 3);
+    free(a);
+    if (split_av != NULL) {
+        i = 0;
+        while (split_av[i] != NULL) {
+            free(split_av[i]);
+            i++;
+        }
+        free(split_av);
+    }
+    return (0);
 }
+
+
+// #include "push_swap.h"
+
+// int	main(int ac, char **av)
+// {
+// 	t_stack	*a;
+// 	t_stack	*b;
+// 	char	*next_line;
+// 	int		len;
+
+// 	a = NULL;
+// 	b = NULL;
+// 	next_line = NULL;
+// 	if (ac == 1)
+// 		return (1);
+// 	else if (ac == 2)
+// 		av = split(av[1], ' ');
+// 	init_stack_a(&a, av + 1);
+// 	len = stack_len(a);
+// 	next_line = (get_next_line(0, next_line));
+// 	while (next_line)
+// 	{
+// 		apply_command(&a, &b, next_line);
+// 		next_line = (get_next_line(0, next_line));
+// 	}
+// 	if (stack_sorted(a) && stack_len(a) == len)
+// 		write(1, "OK\n", 3);
+// 	else
+// 		write(1, "KO\n", 3);
+// 	free(a);
+// }
